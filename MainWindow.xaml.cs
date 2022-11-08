e0a088db940feb479efcf3937e364c6e7fbb82a4
pangy001 <ycpang422@gmail.com>
2022-11-08 10:23:47 +0800

    Add files via upload

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfApp2
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        IList<Student> Students = null;
+        private int counter = 55000;
+        public MainWindow()
+        {
+            InitializeComponent();
+            Students = StudentGenerator.GetStudentList(counter);
+            LVBindingSource();
+        }
+
+        private void LVBindingSource()
+        {
+            this.lvStudents.ItemsSource = Students;            
+            var collectionView = CollectionViewSource.GetDefaultView(Students);
+            collectionView.GroupDescriptions.Add(new PropertyGroupDescription("ClassID"));            
+        }
+    }
+}
---
+++ Students.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WpfApp2
+{
+    public class Student
+    {
+        public int ID { get; set; }
+        public int ClassID { get; set; }
+        public string Name { get; set; }
+    }
+
+    public class StudentGenerator
+    {
+        public static IList<Student> GetStudentList(int count)
+        {
+            IList<Student> students = new List<Student>();
+            for(int i = 0; i < count; i++)
+            {
+                if(i < 100)
+                {
+                    students.Add(new Student { ID = i, ClassID = i % 3, Name = $"stu{i}" });
+                }
+                else
+                {
+                    students.Add(new Student { ID = i, ClassID = 0, Name = $"stu{i}" });
+                }
+                
+            }
+
+            return students;
+        }
+    }
+}
